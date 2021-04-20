5ab22dd55266fee43fd896ea62af48e96b3dfb87
Vitaly Lotnik <v_lotnik@ntc-raduga.ru>
2021-04-20 23:06:18 +0300

    _demo_ test_zero_project
    Run "build_vivado.tcl" to create project.

---
+++ _demo/test_zero_project/build_vivado.tcl
@@ -0,0 +1,28 @@
+set project_name "test_zero_project"
+
+set project_found [llength [get_projects $project_name] ]
+if {$project_found > 0} close_project
+
+# set the reference directory to where the script is
+set origin_dir [file dirname [info script]]
+cd [file dirname [info script]]
+
+# create project
+create_project $project_name "$project_name" -force
+
+# path to the sources
+set src_path ../..
+
+# add sources
+add_files -norecurse tb_top.sv
+add_files -norecurse $src_path/common/common_macros.svh
+add_files -norecurse $src_path/DSP/libraries/pkg_logo.sv
+add_files -norecurse $src_path/DSP/libraries/pkg_modem.sv
+
+# uvm
+set_property -name {xsim.compile.xvlog.more_options} -value {-L uvm} -objects [get_filesets sim_1]
+set_property -name {xsim.elaborate.xelab.more_options} -value {-L uvm} -objects [get_filesets sim_1]
+
+# lunch simulation
+# set_property top tb_top [get_filesets sim_1]
+# launch_simulation
