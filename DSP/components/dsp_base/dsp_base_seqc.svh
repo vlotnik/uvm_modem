8c4763997136c5f1c81a44bb62fd3cbfdaee15eb
Vitaly Lotnik <v_lotnik@ntc-raduga.ru>
2021-04-20 06:00:09 +0300

    [dsp_base_seqc] base sequence
    Add settings:
     - tr_frsz : transaction frame size
     - tr_mod : transaction main modulation
    
    Add output transaction.
    Add function copy_item() : parse data and settings from input transaction to the output transaction.

--- DSP/components/dsp_base/dsp_base_seqc.svh
+++ DSP/components/dsp_base/dsp_base_seqc.svh
@@ -6,11 +6,18 @@ class dsp_base_seqc extends uvm_sequence #(datagen_seqi);
 
     // base transaction
     datagen_seqi                    datagen_seqi_i;
+    datagen_seqi                    datagen_seqi_o;
 
-    // settings
-    int tr_pldsz = 1024;
+    extern function void copy_item();
+
+    // create bit stream
     bit random_data = 1;
     bit bitstream[];
+
+    // settings
+    int                             tr_pldsz = 1024;
+    int                             tr_frsz = 0;
+    t_modulation                    tr_mod = BPSK;
 endclass
 
 //--------------------------------------------------------------------------------------------------------------------------------
@@ -19,15 +26,21 @@ endclass
 task dsp_base_seqc::body();
     `uvm_object_create(datagen_seqi, datagen_seqi_i)
 
-    if (random_data == 1)
+    if (random_data == 1) begin
         assert(datagen_seqi_i.randomize() with {
                 bitstream.size() inside {tr_pldsz};
             });
-    else
+        datagen_seqi_i.tr_pldsz = tr_pldsz;
+    end else begin
         datagen_seqi_i.bitstream = this.bitstream;
-
-    datagen_seqi_i.tr_pldsz = tr_pldsz;
+        datagen_seqi_i.tr_pldsz = this.bitstream.size;
+    end
 
     start_item(datagen_seqi_i);
     finish_item(datagen_seqi_i);
 endtask
+
+function void dsp_base_seqc::copy_item();
+    `uvm_object_create(datagen_seqi, datagen_seqi_o)
+    datagen_seqi_o.copy(datagen_seqi_i);
+endfunction
