f430c437b82db0b39463efa9d2ecfd5f21a9ccfe
Vitaly Lotnik <v_lotnik@ntc-raduga.ru>
2021-04-20 05:49:37 +0300

    [datagen_seqi] add modulation to transaction
    Data:
     - iq_sym[] : array of symbols
    
    Settings:
     - tr_frsz : transaction frame size
     - tr_mod : transaction main modulation
    
    Functions:
     - do_copy : copy data and settings

--- DSP/components/data_generators/datagen_seqi.svh
+++ DSP/components/data_generators/datagen_seqi.svh
@@ -4,11 +4,30 @@ class datagen_seqi extends uvm_sequence_item;
 
     // data
     rand bit                        bitstream[];
+    int                             iq_sym[];
 
     // settings
-    int                             tr_pldsz = 0;
+    int                             tr_pldsz = 0;           // payload size
+    int                             tr_frsz = 0;            // frames ize
+    t_modulation                    tr_mod = BPSK;          // modulation
+
+    extern function void do_copy(uvm_object rhs);
 endclass
 
 //--------------------------------------------------------------------------------------------------------------------------------
 // IMPLEMENTATION
 //--------------------------------------------------------------------------------------------------------------------------------
+function void datagen_seqi::do_copy(uvm_object rhs);
+    datagen_seqi that;
+
+    if (!$cast(that, rhs)) begin
+        `uvm_fatal(get_name(), "Type cast error");
+    end
+
+    super.do_copy(rhs);
+    this.bitstream                  = that.bitstream;
+    this.iq_sym                     = that.iq_sym;
+    this.tr_pldsz                   = that.tr_pldsz;
+    this.tr_frsz                    = that.tr_frsz;
+    this.tr_mod                     = that.tr_mod;
+endfunction
