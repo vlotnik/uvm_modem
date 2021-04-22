71cc0d1d53990f3a4485a854c45a42e1ae9bd899
Vitaly Lotnik <v_lotnik@ntc-raduga.ru>
2021-04-22 08:17:16 +0300

    [pkg_vrf_dsp_components]
    Add:
     - dsp_base_layr
     - dsp_seqc_sym_framer
     - dsp_layr_sym_framer

--- DSP/pkg_vrf_dsp_components.sv
+++ DSP/pkg_vrf_dsp_components.sv
@@ -3,10 +3,17 @@ package pkg_vrf_dsp_components;
     `include "uvm_macros.svh"
     `include "common_macros.svh"
 
+    import pkg_modem::*;
+
     // data generators
     `include "./components/data_generators/datagen_seqi.svh"
     typedef uvm_sequencer #(datagen_seqi) datagen_seqr;
 
     // dsp base
     `include "./components/dsp_base/dsp_base_seqc.svh"
+    `include "./components/dsp_base/dsp_base_layr.svh"
+
+    // symbol framer
+    `include "./components/data_generators/dsp_seqc_sym_framer.svh"
+    `include "./components/data_generators/dsp_layr_sym_framer.svh"
 endpackage
