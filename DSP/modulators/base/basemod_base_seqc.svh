0f3105581e80b410ae6125610bd82b71cbef03df
Vitaly Lotnik <v_lotnik@ntc-raduga.ru>
2021-04-22 22:59:28 +0300

    [basemod_base_seqc] base sequence for base environment
    Translate settings:
     - tr_pldsz : payload size
     - tr_mod : modulation

---
+++ DSP/modulators/base/basemod_base_seqc.svh
@@ -0,0 +1,25 @@
+//--------------------------------------------------------------------------------------------------------------------------------
+// name : basemod_base_seqc
+//--------------------------------------------------------------------------------------------------------------------------------
+class basemod_base_seqc extends dsp_base_seqc;
+    `uvm_object_utils(basemod_base_seqc)
+    `uvm_object_new
+
+    extern task body();
+
+    dsp_base_seqc                   dsp_base_seqc_h;
+endclass
+
+//--------------------------------------------------------------------------------------------------------------------------------
+// IMPLEMENTATION
+//--------------------------------------------------------------------------------------------------------------------------------
+task basemod_base_seqc::body();
+    forever begin
+        `uvm_object_create(dsp_base_seqc, dsp_base_seqc_h)
+
+        dsp_base_seqc_h.tr_pldsz    = tr_pldsz;
+        dsp_base_seqc_h.tr_mod      = tr_mod;
+
+        dsp_base_seqc_h.start(datagen_seqr_h, this);
+    end
+endtask
