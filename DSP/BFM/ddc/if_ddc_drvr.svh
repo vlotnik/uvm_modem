98bc127ec2f05f669dae9135aecb088214079482
Vitaly Lotnik <v_lotnik@ntc-raduga.ru>
2021-05-16 09:52:45 +0300

    [if_ddc_drvr] display I/Q arrays

--- DSP/BFM/ddc/if_ddc_drvr.svh
+++ DSP/BFM/ddc/if_ddc_drvr.svh
@@ -1,3 +1,6 @@
+//--------------------------------------------------------------------------------------------------------------------------------
+// name : if_ddc_drvr
+//--------------------------------------------------------------------------------------------------------------------------------
 class if_ddc_drvr #(
           INCH_NUMBER = 1
         , SIGNAL_TYPE = 1
@@ -20,12 +23,12 @@ task if_ddc_drvr::run_phase(uvm_phase phase);
     datagen_seqi                    datagen_seqi_h;
 
     forever begin
-        @(posedge if_ddc_bfm_h.clk);
-
         seq_item_port.get_next_item(datagen_seqi_h);
             $display("modulation: %s", pkg_modem::get_modulation_settings(datagen_seqi_h.tr_mod).name);
             $display("bitstream: %p", datagen_seqi_h.bitstream);
             $display("symbols: %p", datagen_seqi_h.iq_sym);
+            $display("IQ I: %p", datagen_seqi_h.iq_i);
+            $display("IQ Q: %p", datagen_seqi_h.iq_q);
             $display("\n");
         seq_item_port.item_done();
     end
