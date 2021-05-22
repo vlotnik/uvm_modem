4c787b6836996d9be14d7e387dc7637b52d20a24
Vitaly Lotnik <v_lotnik@ntc-raduga.ru>
2021-05-22 15:35:26 +0300

    [dsp_layr_resampler] resampler : base template for a layer

---
+++ DSP/components/filters/dsp_layr_resampler.svh
@@ -0,0 +1,28 @@
+class dsp_layr_resampler extends dsp_base_layr;
+    `uvm_component_utils(dsp_layr_resampler)
+    `uvm_component_new
+
+    extern function void build_phase(uvm_phase phase);
+    extern function void connect_phase(uvm_phase phase);
+    extern task run_phase(uvm_phase phase);
+
+    // sequence
+    dsp_seqc_resampler              dsp_seqc_resampler_h;
+endclass
+
+//--------------------------------------------------------------------------------------------------------------------------------
+// IMPLEMENTATION
+//--------------------------------------------------------------------------------------------------------------------------------
+function void dsp_layr_resampler::build_phase(uvm_phase phase);
+    super.build_phase(phase);
+
+    `uvm_component_create(dsp_seqc_resampler, dsp_seqc_resampler_h)
+endfunction
+
+function void dsp_layr_resampler::connect_phase(uvm_phase phase);
+    dsp_seqc_resampler_h.datagen_seqr_h = datagen_seqr_i;
+endfunction
+
+task dsp_layr_resampler::run_phase(uvm_phase phase);
+    dsp_seqc_resampler_h.start(datagen_seqr_o);
+endtask
