9044d86e424c0c236693144b31ba27c8d88b3d7b
Vitaly Lotnik <v_lotnik@ntc-raduga.ru>
2021-05-22 16:28:06 +0300

    [datagen_seqi] add frequencies
    Settings:
     - tr_sym_f : transaction symbol frequency
     - tr_rsmp_f : transaction resampling frequency

--- DSP/components/data_generators/datagen_seqi.svh
+++ DSP/components/data_generators/datagen_seqi.svh
@@ -13,8 +13,10 @@ class datagen_seqi extends uvm_sequence_item;
 
     // settings
     int                             tr_pldsz = 0;           // payload size
-    int                             tr_frsz = 0;            // frames ize
+    int                             tr_frsz = 0;            // frames size
     t_modulation                    tr_mod = BPSK;          // modulation
+    real                            tr_sym_f = 1.0;         // symbol frequency
+    real                            tr_rsmp_f = 2.0;        // resampling frequency
 
     extern function void do_copy(uvm_object rhs);
 endclass
@@ -37,4 +39,6 @@ function void datagen_seqi::do_copy(uvm_object rhs);
     this.tr_pldsz                   = that.tr_pldsz;
     this.tr_frsz                    = that.tr_frsz;
     this.tr_mod                     = that.tr_mod;
+    this.tr_sym_f                   = that.tr_sym_f;
+    this.tr_rsmp_f                  = that.tr_rsmp_f;
 endfunction
