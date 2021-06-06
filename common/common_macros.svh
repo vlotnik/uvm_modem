0b56cfb172d45875814c98aa500a8c94644b1b09
Vitaly Lotnik <v_lotnik@ntc-raduga.ru>
2021-06-06 09:33:55 +0300

    [common_macros] add dumper
    dsp_dump_to_file(_nofbytes_, _name_, _v_, _d_, _clk_, _folder_ = "", _id_ = 0, _use_id_ = 0)
     - _nofbytes_ : number of bytes to be written
     - _name_ : output file name
     - _v_ : data valid
     - _d_ : data to be written
     - _clk_ : data clock
     - _folder_ : output folder name (by default "")
     - _id_ : output file name will ends with id (by default _0)
     - _use_id_ : must be set to 1 if you want to use _id_

--- common/common_macros.svh
+++ common/common_macros.svh
@@ -1,3 +1,6 @@
+//--------------------------------------------------------------------------------------------------------------------------------
+// name : common_macros
+//--------------------------------------------------------------------------------------------------------------------------------
 `ifndef print_logo
     `define print_logo \
         int logo_sel; \
@@ -29,4 +32,22 @@
 `ifndef uvm_component_create
     `define uvm_component_create(_type_name_, _inst_name_, _id_ = 0) \
         _inst_name_ = _type_name_::type_id::create($sformatf({`"_inst_name_`", "_%0d"}, _id_), this);
+`endif
+
+// dump data to file
+`ifndef dsp_dump_to_file
+    `define dsp_dump_to_file(_nofbytes_, _name_, _v_, _d_, _clk_, _folder_ = "", _id_ = 0, _use_id_ = 0) \
+    file_io #(_nofbytes_) file_``_name_; \
+    initial begin \
+        if (_use_id_ == 1) \
+            file_``_name_ = new({_folder_, $sformatf({`"_name_`", "_%0d"}, _id_), ".bin"}, "wb"); \
+        else \
+            file_``_name_ = new({_folder_, `"_name_`", ".bin"}, "wb"); \
+    end \
+    always @(posedge _clk_) \
+    begin : _name_ \
+        if (_v_ == 1) begin \
+            file_``_name_.write(_d_); \
+        end \
+    end
 `endif
