5a9aa2f7d85818afc1423c09e754987cc3ada7b8
Vitaly Lotnik <v_lotnik@ntc-raduga.ru>
2021-06-11 23:24:27 +0300

    [basemod_base_seqc]
    Translate settings:
     - tr_sym_f : symbol frequency
     - tr_rsmp_f : resampling frequency
     - tr_car_f : carrier frequency

--- DSP/modulators/base/basemod_base_seqc.svh
+++ DSP/modulators/base/basemod_base_seqc.svh
@@ -19,6 +19,9 @@ task basemod_base_seqc::body();
 
         dsp_base_seqc_h.tr_pldsz    = tr_pldsz;
         dsp_base_seqc_h.tr_mod      = tr_mod;
+        dsp_base_seqc_h.tr_sym_f    = tr_sym_f;
+        dsp_base_seqc_h.tr_rsmp_f   = tr_rsmp_f;
+        dsp_base_seqc_h.tr_car_f    = tr_car_f;
 
         dsp_base_seqc_h.start(datagen_seqr_h, this);
     end
