a86c5efb365d795b47bc2c98c745d5fb28c80763
Vitaly Lotnik <v_lotnik@ntc-raduga.ru>
2021-06-11 23:21:14 +0300

    [dsp_seqc_mixer] complex mixer implementation

--- DSP/components/dsp_math/dsp_seqc_mixer.svh
+++ DSP/components/dsp_math/dsp_seqc_mixer.svh
@@ -6,15 +6,21 @@ class dsp_seqc_mixer extends dsp_base_seqc;
     `uvm_object_new
 
     extern task body();
+
+    dspmath_mixer                   dspmath_mixer_h;
 endclass
 
 //--------------------------------------------------------------------------------------------------------------------------------
 // IMPLEMENTATION
 //--------------------------------------------------------------------------------------------------------------------------------
 task dsp_seqc_mixer::body();
+    `uvm_object_create(dspmath_mixer, dspmath_mixer_h)
+
     forever begin
         datagen_seqr_h.get_next_item(datagen_seqi_i);
         copy_item();
+
+        dspmath_mixer_h.mix(datagen_seqi_o);
 
         start_item(datagen_seqi_o);
         finish_item(datagen_seqi_o);
