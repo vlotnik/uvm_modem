b57f11d0dc6b11fc6d6d31c2aab91192b9cbd712
Vitaly Lotnik <v_lotnik@ntc-raduga.ru>
2021-06-28 20:57:46 +0300

    [datagen_seqi]
    Settings:
     - tr_last : could be set to 1 in the last transaction
    
    Functions:
     - new_seqi() : creates a new transaction with a given length

--- DSP/components/data_generators/datagen_seqi.svh
+++ DSP/components/data_generators/datagen_seqi.svh
@@ -12,6 +12,7 @@ class datagen_seqi extends uvm_sequence_item;
     real                            iq_q[];
 
     // settings
+    bit                             tr_last = 0;
     int                             tr_pldsz = 0;           // payload size
     int                             tr_frsz = 0;            // frames size
     t_modulation                    tr_mod = BPSK;          // modulation
@@ -19,12 +20,19 @@ class datagen_seqi extends uvm_sequence_item;
     real                            tr_rsmp_f = 2.0;        // resampling frequency
     real                            tr_car_f = 0.0;         // carrier frequency
 
+    extern function void new_seqi(int size);
     extern function void do_copy(uvm_object rhs);
 endclass
 
 //--------------------------------------------------------------------------------------------------------------------------------
 // IMPLEMENTATION
 //--------------------------------------------------------------------------------------------------------------------------------
+function void datagen_seqi::new_seqi(int size);
+    iq_sym                          = new[size];
+    iq_i                            = new[size];
+    iq_q                            = new[size];
+endfunction
+
 function void datagen_seqi::do_copy(uvm_object rhs);
     datagen_seqi that;
 
@@ -37,6 +45,7 @@ function void datagen_seqi::do_copy(uvm_object rhs);
     this.iq_sym                     = that.iq_sym;
     this.iq_i                       = that.iq_i;
     this.iq_q                       = that.iq_q;
+    this.tr_last                    = that.tr_last;
     this.tr_pldsz                   = that.tr_pldsz;
     this.tr_frsz                    = that.tr_frsz;
     this.tr_mod                     = that.tr_mod;
