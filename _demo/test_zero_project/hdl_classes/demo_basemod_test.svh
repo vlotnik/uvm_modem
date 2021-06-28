07d8ef3181ca61e3cf341ff4e67db585670f7fc0
Vitaly Lotnik <v_lotnik@ntc-raduga.ru>
2021-06-28 21:19:24 +0300

    _demo_ test_zero_project
    4-channel test

--- _demo/test_zero_project/hdl_classes/demo_basemod_test.svh
+++ _demo/test_zero_project/hdl_classes/demo_basemod_test.svh
@@ -3,6 +3,7 @@ class demo_basemod_test extends uvm_test;
     `uvm_component_new
 
     localparam INCH_NUMBER = 2;
+    localparam NOFCH = 4;
 
     // functions
     extern function void build_phase(uvm_phase phase);
@@ -10,10 +11,15 @@ class demo_basemod_test extends uvm_test;
     extern task run_phase(uvm_phase phase);
 
     // sequence
-    basemod_base_seqc               seqc_h;
+    basemod_base_seqc               seqc_h[NOFCH];
 
     // modulator
-    basemod_envr                    basemod_envr_h;
+    basemod_envr                    basemod_envr_h[NOFCH];
+
+    // summator
+    dsp_layr_summator #(
+          NOFCH
+    )                               dsp_summ_h;
 
     // interfaces
     virtual if_ddc_bfm #(
@@ -22,6 +28,10 @@ class demo_basemod_test extends uvm_test;
     if_ddc_agnt #(
           .INCH_NUMBER(INCH_NUMBER)
     )                               if_ddc_agnt_h;
+
+    // settings
+    real sym_f[NOFCH];
+    real car_f[NOFCH];
 endclass
 
 //--------------------------------------------------------------------------------------------------------------------------------
@@ -32,16 +42,26 @@ function void demo_basemod_test::build_phase(uvm_phase phase);
     if (!uvm_config_db #(virtual if_ddc_bfm #(INCH_NUMBER))::get(this, "", "if_ddc_bfm_h", if_ddc_bfm_h)
     )`uvm_fatal("BFM", "Failed to get BFM");
 
-    // modulator
-    `uvm_object_create(basemod_base_seqc, seqc_h)
-    seqc_h.tr_pldsz                 = 100;
-    seqc_h.tr_mod                   = QPSK;
-    seqc_h.tr_sym_f                 = 1.0;
-    seqc_h.tr_rsmp_f                = 8.0;
-    seqc_h.tr_car_f                 = 2.0;
+    // calculate settings
+    for (int ii = 0; ii < NOFCH; ii++) begin
+        sym_f[ii] = 1.0;
+        car_f[ii] = -3 + 2 * ii;
+    end
 
     // modulator
-    `uvm_component_create(basemod_envr, basemod_envr_h)
+    for (int ii = 0; ii < NOFCH; ii++) begin
+        `uvm_object_create(basemod_base_seqc, seqc_h[ii], ii)
+        seqc_h[ii].tr_pldsz  = 100;
+        seqc_h[ii].tr_mod    = QPSK;
+        seqc_h[ii].tr_sym_f  = sym_f[ii];
+        seqc_h[ii].tr_rsmp_f = 8.0;
+        seqc_h[ii].tr_car_f  = car_f[ii];
+
+        `uvm_component_create(basemod_envr, basemod_envr_h[ii], ii)
+    end
+
+    // build summator
+    `uvm_component_create(dsp_layr_summator #(NOFCH), dsp_summ_h)
 
     // agent
     `uvm_component_create(if_ddc_agnt #(INCH_NUMBER), if_ddc_agnt_h);
@@ -49,17 +69,24 @@ function void demo_basemod_test::build_phase(uvm_phase phase);
 endfunction
 
 function void demo_basemod_test::connect_phase(uvm_phase phase);
-    seqc_h.datagen_seqr_h = basemod_envr_h.datagen_seqr_i;
-    // basemod_envr_h.dsp_symf_h.datagen_seqr_o = if_ddc_agnt_h.datagen_seqr_h;
-    // basemod_envr_h.dsp_mapp_h.datagen_seqr_o = if_ddc_agnt_h.datagen_seqr_h;
-    // basemod_envr_h.dsp_rsmp_h.datagen_seqr_o = if_ddc_agnt_h.datagen_seqr_h;
-    basemod_envr_h.dsp_mixr_h.datagen_seqr_o = if_ddc_agnt_h.datagen_seqr_h;
+    for (int ii = 0; ii < NOFCH; ii++) begin
+        seqc_h[ii].datagen_seqr_h = basemod_envr_h[ii].datagen_seqr_i;
+        basemod_envr_h[ii].dsp_mixr_h.datagen_seqr_o = dsp_summ_h.datagen_seqr_i[ii];
+    end
+
+    dsp_summ_h.datagen_seqr_o = if_ddc_agnt_h.datagen_seqr_h;
 endfunction
 
 task demo_basemod_test::run_phase(uvm_phase phase);
     phase.raise_objection(this);
 
-        seqc_h.start(null);
+        for (int ii = 0; ii < NOFCH; ii++) begin
+            automatic int iia = ii;
+            fork
+                seqc_h[iia].start(null);
+            join_none
+        end
+        wait fork;
 
     phase.drop_objection(this);
 endtask
