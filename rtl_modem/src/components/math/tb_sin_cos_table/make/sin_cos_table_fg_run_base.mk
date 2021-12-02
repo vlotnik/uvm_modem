f9990beb6222646cd0039f4349f8eed2cb8b21b9
Vitaly Lotnik <vlotnik@gmail.com>
2021-12-02 18:39:53 +0300

    [sin_cos_table] fix run base makefile

--- rtl_modem/src/components/math/tb_sin_cos_table/make/sin_cos_table_fg_run_base.mk
+++ rtl_modem/src/components/math/tb_sin_cos_table/make/sin_cos_table_fg_run_base.mk
@@ -1,7 +1,6 @@
 ## | base
 
-base_run_sim:
-	vsim -64 -c -voptargs="+acc" \
+BASE_RUN_SIM_CMD = \
 	-g GPDW=${CONFIG_GPDW} \
 	-g FULL_TABLE=${CONFIG_FULL_TABLE} \
 	-g PHASE_W=${CONFIG_PHASE_W} \
@@ -9,7 +8,7 @@ base_run_sim:
 	-g PIPE_CE=${CONFIG_PIPE_CE} \
 	-L rtl_modem \
 	-L ${DEF_LIB} \
-	-sv_seed random \
+	-sv_seed ${CONFIG_SV_SEED} \
 	${DEF_LIB}.tb_sin_cos_table \
 	+UVM_VERBOSITY=UVM_FULL \
 	+UVM_TESTNAME=sincos_base_test \
@@ -17,22 +16,13 @@ base_run_sim:
 	-do "run -all" \
 	-do "quit"
 
+base_run_sim:
+	vsim -64 -c -voptargs="+acc" \
+	${BASE_RUN_SIM_CMD}
+
 base_run_sim_gui:
 	vsim -64 -voptargs="+acc" \
-	-g GPDW=${CONFIG_GPDW} \
-	-g FULL_TABLE=${CONFIG_FULL_TABLE} \
-	-g PHASE_W=${CONFIG_PHASE_W} \
-	-g SINCOS_W=${CONFIG_SINCOS_W} \
-	-g PIPE_CE=${CONFIG_PIPE_CE} \
-	-L rtl_modem \
-	-L ${DEF_LIB} \
-	-sv_seed random \
-	${DEF_LIB}.tb_sin_cos_table \
-	+UVM_VERBOSITY=UVM_HIGH \
-	+UVM_TESTNAME=sincos_base_test \
-	-do "wave.do" \
-	-do "run -all" \
-	-do "quit"
+	${BASE_RUN_SIM_CMD}
 
 base_uvm: \
 	comp_sv \
