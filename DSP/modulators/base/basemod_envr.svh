8a6e8e16d5ba42cc4c3ae7f03300ef5f3ad082e2
Vitaly Lotnik <vlotnik@gmail.com>
2022-01-23 20:09:05 +0300

    [basemod_envr] refactoring 2022

--- DSP/modulators/base/basemod_envr.svh
+++ DSP/modulators/base/basemod_envr.svh
@@ -8,13 +8,13 @@ class basemod_envr extends uvm_env;
     extern function void build_phase(uvm_phase phase);
     extern function void connect_phase(uvm_phase phase);
 
-    datagen_seqr                    datagen_seqr_i;
+    datagen_seqr                        datagen_seqr_i;
 
     // layers
-    dsp_layr_sym_framer             dsp_symf_h;
-    dsp_layr_iq_mapper              dsp_mapp_h;
-    dsp_layr_resampler              dsp_rsmp_h;
-    dsp_layr_mixer                  dsp_mixr_h;
+    dsp_layr_sym_framer                 dsp_symf_h;
+    dsp_layr_iq_mapper                  dsp_mapp_h;
+    dsp_layr_resampler                  dsp_rsmp_h;
+    dsp_layr_mixer                      dsp_mixr_h;
 endclass
 
 //--------------------------------------------------------------------------------------------------------------------------------
