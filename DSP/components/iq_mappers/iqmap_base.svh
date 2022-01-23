124f3578b62b060b071808209dd9d06b67195c17
Vitaly Lotnik <vlotnik@gmail.com>
2022-01-23 20:12:46 +0300

    [iqmap_base] refactoring 2022

--- DSP/components/iq_mappers/iqmap_base.svh
+++ DSP/components/iq_mappers/iqmap_base.svh
@@ -12,6 +12,9 @@ class iqmap_base extends uvm_object;
     // this array will be used to fill I/Q data
     real plane[];
 
+    // scale
+    int iq_scale = 724;
+
     virtual function void init_plane(t_modulation mod);
     endfunction
 
@@ -35,7 +38,7 @@ function void iqmap_base::get_iq(ref datagen_seqi datagen_seqi_h);
 
     // fill I/Q data
     for (int ii = 0; ii < tr_frsz; ii++) begin
-        datagen_seqi_h.iq_i[ii] = plane[2 * datagen_seqi_h.iq_sym[ii] + 0];
-        datagen_seqi_h.iq_q[ii] = plane[2 * datagen_seqi_h.iq_sym[ii] + 1];
+        datagen_seqi_h.iq_i[ii] = $rtoi(iq_scale * plane[2 * datagen_seqi_h.iq_sym[ii] + 0]);
+        datagen_seqi_h.iq_q[ii] = $rtoi(iq_scale * plane[2 * datagen_seqi_h.iq_sym[ii] + 1]);
     end
 endfunction
