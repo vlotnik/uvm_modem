c58564f0e0ddb747851e6bd0a1259e989534db1e
Vitaly Lotnik <vlotnik@gmail.com>
2022-02-20 10:39:45 +0300

    [BUILD]

--- scripts/list_of_tests.py
+++ scripts/list_of_tests.py
@@ -14,6 +14,7 @@ def get_list_of_tests():
     # build and simulate
     tb_path.append([1, current_path + "/rtl_modem/src/components/math/tb_sin_cos_table"])
     tb_path.append([1, current_path + "/rtl_modem/src/components/math/tb_complex_multiplier"])
+    tb_path.append([1, current_path + "/rtl_modem/src/components/math/tb_complex_rotator"])
     tb_path.append([1, current_path + "/rtl_modem/src/components/filters/tb_simple_fir_filter"])
 
     return tb_path
